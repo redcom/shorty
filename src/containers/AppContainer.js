6e00916e6431031feaf9494ae7ccb0792fd66319
Razvan Moraru <razvan@inclub.ro>
2017-03-29 23:27:10 +0200

    feat: create AppContainer

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,28 +1,10 @@
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Wrapper, FormUrlShorter } from '../components';
-import * as AppActions from '../actions/AppActions';
 
-class AppContainer extends React.Component {
-  render() {
-    return (
-      <Wrapper>
-        <FormUrlShorter />
-      </Wrapper>
-    );
-  }
-}
+const AppContainer = () => (
+  <Wrapper>
+    <FormUrlShorter />
+  </Wrapper>
+);
 
-AppContainer.propTypes = {
-  increment: PropTypes.func,
-  decrement: PropTypes.func,
-  incrementIfOdd: PropTypes.func,
-};
-
-function mapStateToProps(state) {
-  return {
-    links: state.links,
-  };
-}
-
-export default connect(mapStateToProps, { ...AppActions })(AppContainer);
+export default AppContainer;
